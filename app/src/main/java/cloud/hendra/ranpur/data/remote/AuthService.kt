41265ec5f606e0b715f26b0ea88061e59c0de0d6
Hendra Dwi Saputra <hendra@usa.com>
2025-03-19 07:57:46 +0700

    Introduce AuthService interface for handling authentication
    This commit introduces the `AuthService` interface, which is designed to handle authentication-related network requests.
    - Adds `AuthService.kt` to define the `AuthService` interface.
    - The `AuthService` interface includes a `login` function annotated with `@POST` to handle login requests.
    - The `login` function takes a `LoginRequest` as input and returns a `Response<LoginResponse>`.
    - Uses Retrofit annotations like `@POST` and `@Body` to define the HTTP request details.

---
+++ app/src/main/java/cloud/hendra/ranpur/data/remote/AuthService.kt
@@ -0,0 +1,12 @@
+package cloud.hendra.ranpur.data.remote
+
+import cloud.hendra.ranpur.data.remote.dto.LoginRequest
+import cloud.hendra.ranpur.data.remote.dto.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("api/v1/login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+}
