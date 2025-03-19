c6286bd73b796f55135d466f88932dfdbca1762d
Hendra Dwi Saputra <hendra@usa.com>
2025-03-19 23:31:32 +0700

    Refactor Sales Page with TopAppBar and OutlinedTextField
    This commit refactors the Sales Page UI using Jetpack Compose.
    - Replaces `TextField` with `OutlinedTextField` for search input.
    - Adds `TopAppBar` with title "Ternak Cuan" and customized colors.
    - Use `Scaffold` for layout structure.
    - The search field uses `searchQuery` to keep track of the text inserted.
    - Adds padding and a `Spacer` for the new layout.
    - Updates colors and text style for enhanced UI.
    - Uses `TopAppBarColors` to update colors.
    - Use `Scaffold` `innerPadding` for padding.
    - Uses `remember` `mutableStateOf` to save the state.

--- app/src/main/java/cloud/hendra/ranpur/ui/screen/SalesPage.kt
+++ app/src/main/java/cloud/hendra/ranpur/ui/screen/SalesPage.kt
@@ -1,7 +1,5 @@
 package cloud.hendra.ranpur.ui.screen
 
-import android.widget.Button
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,110 +8,114 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SalesPage() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            TextField(
-                value = "",
-                onValueChange = {},
-                modifier = Modifier
-                    .weight(1f)
-                    .height(54.dp)
-                    .clip(RoundedCornerShape(8.dp)),
-                placeholder = { Text("Search...") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "Search"
+    var searchQuery by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Ternak Cuan".uppercase(),
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 18.sp,
                     )
                 },
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Button(
-                onClick = { /* Handle search */ },
-                modifier = Modifier.height(48.dp),
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = "Search Button"
+                colors = TopAppBarColors(
+                    titleContentColor = MaterialTheme.colorScheme.inversePrimary,
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    scrolledContainerColor = MaterialTheme.colorScheme.background,
+                    navigationIconContentColor = MaterialTheme.colorScheme.inversePrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onTertiary
                 )
-            }
+            )
         }
-
-        Card (
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(8.dp)
+                .padding(top = 16.dp)
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(Color.White)
         ) {
-            Row(
-                modifier = Modifier.padding(16.dp)
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                leadingIcon = {
+                    Icon(Icons.Filled.Search, contentDescription = "Search")
+                },
+                label = { Text("Search Products") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Card(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
             ) {
-                Column(
-                    modifier = Modifier.weight(1f)
+                Row(
+                    modifier = Modifier.padding(16.dp)
                 ) {
-                    Text(
-                        text = "Nama Produk",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black
-                    )
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "Nama Produk",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black
+                        )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(8.dp))
 
-                    Text(
-                        text = "Rp 1.000.000",
-                        fontSize = 16.sp,
-                        color = Color(0xFF6200EE),
-                        fontWeight = FontWeight.Medium
-                    )
+                        Text(
+                            text = "Rp 1.000.000",
+                            fontSize = 16.sp,
+                            color = Color(0xFF6200EE),
+                            fontWeight = FontWeight.Medium
+                        )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(8.dp))
 
-                    Text(
-                        text = "Stok: 50 pcs",
-                        fontSize = 14.sp,
-                        color = Color.Gray
-                    )
+                        Text(
+                            text = "Stok: 50 pcs",
+                            fontSize = 14.sp,
+                            color = Color.Gray
+                        )
+                    }
                 }
             }
         }
+
     }
 }
 
