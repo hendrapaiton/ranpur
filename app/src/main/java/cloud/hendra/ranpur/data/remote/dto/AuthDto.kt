5bee5cb1b029bee05dedbf355871e5cbe027e79c
Hendra Dwi Saputra <hendra@usa.com>
2025-03-19 07:45:43 +0700

    Add data classes for authentication DTOs
    This commit introduces data transfer objects (DTOs) for handling authentication requests and responses.
    - Creates `AuthDto.kt` to contain the DTOs.
    - Adds `LoginRequest` data class with `email` and `password` properties, annotated with `@SerializedName`.
    - Adds `LoginResponse` data class with a `token` property, annotated with `@SerializedName`.

---
+++ app/src/main/java/cloud/hendra/ranpur/data/remote/dto/AuthDto.kt
@@ -0,0 +1,12 @@
+package cloud.hendra.ranpur.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String
+)
+
+data class LoginResponse(
+    @SerializedName("token") val token: String
+)
