295d93eca6a029ce35bbef688c34c2f9eda2161e
Hendra Dwi Saputra <hendra@usa.com>
2025-03-23 09:15:56 +0700

    Define navigation routes in Routes.kt
    This commit introduces a new `Routes.kt` file to define navigation routes for the application.
    -   `Routes.kt` is created to define constant route names.
    - Defines route for `LOADING`, `PRODUCT`, `LOGIN`, `STOCK`, and `SALES`.
    - Uses object to store the routes name.

---
+++ app/src/main/java/cloud/hendra/ranpur/ui/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package cloud.hendra.ranpur.ui.navigation
+
+object Routes {
+    const val LOADING = "loading"
+    const val PRODUCT = "product"
+    const val LOGIN = "login"
+    const val STOCK = "stock"
+    const val SALES = "sales"
+}
