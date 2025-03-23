805b2ae50c1c256d98496e6bf903377816b1f4d1
Hendra Dwi Saputra <hendra@usa.com>
2025-03-23 09:20:23 +0700

    Refactor navigation routes using constants
    This commit refactors the navigation routes in `Navigation.kt` to use constants instead of hardcoded strings.
    - Introduces `Routes` object to define constant values for "login", "product", "stock", and "sales" routes.
    - Updates `NavHost` to use these constants (`LOGIN`, `PRODUCT`, `STOCK`, `SALES`) for routes and start destination.
    - Updates `LoginPage` navigation to `SALES` route after success.

--- app/src/main/java/cloud/hendra/ranpur/ui/navigation/Navigation.kt
+++ app/src/main/java/cloud/hendra/ranpur/ui/navigation/Navigation.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import cloud.hendra.ranpur.ui.navigation.Routes.LOGIN
+import cloud.hendra.ranpur.ui.navigation.Routes.PRODUCT
+import cloud.hendra.ranpur.ui.navigation.Routes.SALES
+import cloud.hendra.ranpur.ui.navigation.Routes.STOCK
 import cloud.hendra.ranpur.ui.screen.LoginPage
 import cloud.hendra.ranpur.ui.screen.ProductPage
 import cloud.hendra.ranpur.ui.screen.SalesPage
@@ -13,18 +17,18 @@ import cloud.hendra.ranpur.ui.screen.StockPage
 fun Navigation(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = "login"
+        startDestination = LOGIN
     ) {
-        composable(route = "login") {
-            LoginPage(onLoginSuccess = { navController.navigate("sales") })
+        composable(route = LOGIN) {
+            LoginPage(onLoginSuccess = { navController.navigate(SALES) })
         }
-        composable(route = "product") {
+        composable(route = PRODUCT) {
             ProductPage()
         }
-        composable(route = "stock") {
+        composable(route = STOCK) {
             StockPage()
         }
-        composable(route = "sales") {
+        composable(route = SALES) {
             SalesPage()
         }
     }
